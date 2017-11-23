42caf98327febdaa7b0362e2cf59a33e3e96396a
BoyWithSilverWings <agney.menons@live.com>
2017-11-23 23:02:57 +0530

    Adding logger and helmet

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const compression = require('compression');
+const helmet = require('helmet');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const path = require('path');
+const logger = require('morgan');
 
 dotenv.load({ path: '.env' });
 
 const app = express();
+
 app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
 app.use(compression());
+app.use(helmet());
+
+app.use(bodyParser.json());
 
 app.listen(app.get('port'), ()=>{
   console.log(`App running on port ${app.get('port')}`);
-})
+});
